3bf66befe9c6a58cb3985ca8e8cbd48a8661c084
Emily Harbeck <eharbeck@me.com>
2024-09-22 17:29:32 +1000

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+    name: "Barry Humphrey",
+    streetAddress: "12 Mott Street",
+};
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+   
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj,key,value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    delete employee.name.valueOf;
+    delete employee.streetAddress.valueOf;
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key,value) {
+    obj[key] = value;
+    delete employee.name;
+    delete employee.streetAddress;
+
+    return obj;
+    
+}
